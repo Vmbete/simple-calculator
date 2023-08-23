06d8e3ac83f30e1034f8ba883ce4a6ff2358d7ec
vincent mutua <root@Vincent-Laptop>
2023-08-23 16:02:18 +0300

    MOdify add fxn and add first python program file

--- calc.py
+++ calc.py
@@ -1,2 +1,10 @@
 def add_two_integers(a, b):
-    return a + b
+    print(f"Adding {a} {b}")
+    result = a + b
+    print(f"Result: {result}")
+    return result
+
+x = int(input("Enter First Number: "))
+y = int(input("Enter Second Number: "))
+
+add_two_integers(x, y)
